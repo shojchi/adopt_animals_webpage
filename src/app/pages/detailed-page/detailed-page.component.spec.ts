fef3f120dd24c441a3167d68a6cbca61a4c9f395
Roman Hordiichuk <rhordiichuk@gmail.com>
2025-02-11 09:17:54 +0200

    - added tests 3/3

--- src/app/pages/detailed-page/detailed-page.component.spec.ts
+++ src/app/pages/detailed-page/detailed-page.component.spec.ts
@@ -3,38 +3,45 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { DetailedPageComponent } from './detailed-page.component';
 import { ActivatedRoute } from '@angular/router';
 import { of } from 'rxjs';
+import { NotificationsService } from '../../shared/services/notifications/notifications.service';
 
 describe('DetailedPageComponent', () => {
   let component: DetailedPageComponent;
   let fixture: ComponentFixture<DetailedPageComponent>;
+  let notificationsServiceMock: jasmine.SpyObj<NotificationsService>;
 
   beforeEach(async () => {
+    notificationsServiceMock = jasmine.createSpyObj('NotificationsService', ['setSharedData']);
+
     await TestBed.configureTestingModule({
       imports: [DetailedPageComponent],
-      providers: [{
-        provide: ActivatedRoute,
-        useValue: {
-          snapshot: {
-            data: {
-              animalData: {
-                id: 1,
-                name: "Buddy",
-                breed: "Golden Retriever",
-                gender: "male",
-                species: "dog",
-                animalId: "123",
-                age: "2 years",
-                size: "L",
-                intakeDate: "10/01/2023",
-                onHold: false,
-                spayedOrNeutered: true,
-                mainPhotoUrl: "https://example.com/dog.jpg"
+      providers: [
+        { provide: NotificationsService, useValue: notificationsServiceMock },
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              data: {
+                animalData: {
+                  id: 1,
+                  name: "Buddy",
+                  breed: "Golden Retriever",
+                  gender: "male",
+                  species: "dog",
+                  animalId: "123",
+                  age: "2 years",
+                  size: "L",
+                  intakeDate: "10/01/2023",
+                  onHold: false,
+                  spayedOrNeutered: true,
+                  mainPhotoUrl: "https://example.com/dog.jpg"
+                }
               }
-            }
-          },
-          paramMap: of({ get: () => 'mock-id' })
+            },
+            paramMap: of({ get: () => 'mock-id' })
+          }
         }
-      }]
+      ]
     })
       .compileComponents();
 
@@ -45,5 +52,13 @@ describe('DetailedPageComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call makeAdoption and update the state', () => {
+    component.makeAdoption();
+
+    expect(component.showAdoptButton).toBeFalse();
+
+    expect(notificationsServiceMock.setSharedData).toHaveBeenCalledWith('Buddy');
   });
 });
